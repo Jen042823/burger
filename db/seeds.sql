596d5fc365ad234beb84483890078ee4284c05da
Jenny Yoon <yoon042823@gmail.com>
2020-06-15 21:14:53 -0400

    homework wow

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger) VALUES ('cheeseburger');
+INSERT INTO burgers (burger) VALUES ('bbq cheeseburger');
+INSERT INTO burgers (burger, eaten) VALUES ('triple cheeseburger with extra cheese', 1);
